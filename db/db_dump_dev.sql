5f4ac0278c55816dee3e73d394ce0b61eb17b2d6
František Gič <frantisek.gic@gmail.com>
2020-04-13 19:19:06 +0200

    Update dumps with correct laborant_id

--- db/db_dump_dev.sql
+++ db/db_dump_dev.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a0547452f7c788c60a8ec8802463726c615eb771c4377162ec6ca2cf4d9164cc
-size 43379
+oid sha256:3c07370a36d4c1b481593a53f95168b5dd17bbf7548d7dfd98be14ce2f602b5f
+size 183340
